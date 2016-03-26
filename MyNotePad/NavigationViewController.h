f890bd11053a2fbc25a209119fffc885e5c3b541
johnnymartings <yhr577@163.com>
2016-03-26 18:20:30 +0800

    fist commit
    first commit code.

--- MyNotePad/NavigationViewController.h
+++ MyNotePad/NavigationViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NavigationViewController.h
 //  MyNotePad
 //
 //  Created by yang johnny on 3/22/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NavigationViewController : UINavigationController
 
 @end
-
